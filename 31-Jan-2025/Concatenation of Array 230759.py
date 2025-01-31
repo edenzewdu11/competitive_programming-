c4c208c624f46fb81906e116c341ed631114a058
Eden Zewdu <167537470+edenzewdu11@users.noreply.github.com>
2025-01-31 15:08:35 +0300

    Created 'Concatenation of Array' by A2SV Hub

---
+++ 31-Jan-2025/Concatenation of Array 230759.py
@@ -0,0 +1,8 @@
+# Problem: Concatenation of Array - https://leetcode.com/problems/concatenation-of-array/description/
+
+class Solution(object):
+    def getConcatenation(self, nums):
+       nums=nums+nums
+      
+       return nums
+        
