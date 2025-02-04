f14d85f39157d6045eab68b7d51e102b5839caf1
Eden Zewdu <167537470+edenzewdu11@users.noreply.github.com>
2025-02-04 10:30:03 +0300

    Created 'Valid Palindrome' by A2SV Hub

---
+++ 04-Feb-2025/Valid Palindrome 239211.py
@@ -0,0 +1,18 @@
+# Problem: Valid Palindrome - https://leetcode.com/problems/valid-palindrome/description/
+
+class Solution(object):
+    def isPalindrome(self, s):
+        ss = []
+        for i in s:
+            if i.isalnum():
+                ss.append(i.lower())
+        i = 0
+        j = len(ss) - 1
+        while i < j:
+            if ss[i] != ss[j]:
+                return False
+            i += 1
+            j -= 1
+        return True
+
+
