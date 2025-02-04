263e6ca83077e7cb61928e7a2a77c40760559a4c
Eden Zewdu <167537470+edenzewdu11@users.noreply.github.com>
2025-02-04 18:24:58 +0300

    Created 'Valid Anagram' by A2SV Hub

---
+++ 04-Feb-2025/Valid Anagram 240529.py
@@ -0,0 +1,12 @@
+# Problem: Valid Anagram - https://leetcode.com/problems/valid-anagram/description/
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_sort=sorted(s)
+        t_sort=sorted(t)
+        
+        if s_sort==t_sort:
+            return True
+        else:
+            return False
+        
