1fe37494f4cb733e1e0b22ef3ddf7d63f48d02d5
Eden Zewdu <167537470+edenzewdu11@users.noreply.github.com>
2025-02-04 07:34:45 +0300

    Created 'Team' by A2SV Hub

---
+++ 04-Feb-2025/Team 237973.py
@@ -0,0 +1,11 @@
+# Problem: Team - https://codeforces.com/contest/231/problem/A
+
+n = int(input().strip())
+count = 0
+
+for _ in range(n):
+    problem = list(map(int, input().strip().split()))
+    if sum(problem) >= 2:
+        count += 1
+
+print(count)
