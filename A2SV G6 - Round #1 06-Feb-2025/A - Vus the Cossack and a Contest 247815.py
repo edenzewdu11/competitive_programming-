42c4600bec59a0b31895b00b38e0c5826306b7a3
Eden Zewdu <167537470+edenzewdu11@users.noreply.github.com>
2025-02-06 20:42:02 +0300

    Created 'A - Vus the Cossack and a Contest' by A2SV Hub

---
+++ A2SV G6 - Round #1 06-Feb-2025/A - Vus the Cossack and a Contest 247815.py
@@ -0,0 +1,7 @@
+# Problem: A - Vus the Cossack and a Contest - https://codeforces.com/gym/585107/problem/A
+
+n,m,k=list(map(int,input().split()))
+if n<=m and n<=k:
+    print("Yes")
+else:
+    print("No")
