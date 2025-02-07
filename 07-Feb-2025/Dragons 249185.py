edb4649483a1ce47997d74cf0d3572f384801d0d
Eden Zewdu <167537470+edenzewdu11@users.noreply.github.com>
2025-02-07 11:02:22 +0300

    Created 'Dragons' by A2SV Hub

---
+++ 07-Feb-2025/Dragons 249185.py
@@ -0,0 +1,18 @@
+# Problem: Dragons - https://codeforces.com/problemset/problem/230/A
+
+s, n = map(int, input().split())
+dragons = []
+for _ in range(n):
+    xi, yi = map(int, input().split())
+    dragons.append((xi, yi))
+dragons.sort()
+ 
+for xi, yi in dragons:
+    if s > xi:
+        s += yi 
+    else:
+        print("NO")
+        break
+else:
+    
+    print("YES")
