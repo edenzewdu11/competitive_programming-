e5d79e4511db8a755ba7d678e3f61dd8aa7a203a
Eden Zewdu <167537470+edenzewdu11@users.noreply.github.com>
2025-02-19 14:23:01 +0300

    Created 'Shifting Letters II' by A2SV Hub

---
+++ 19-Feb-2025/Shifting Letters II 273427.py
@@ -0,0 +1,23 @@
+# Problem: Shifting Letters II - https://leetcode.com/problems/shifting-letters-ii/description/
+
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+        prefix=[0]*len(s)
+        for shift in shifts:
+            left,right,dirn=shift
+            if dirn==0:
+                prefix[left] +=-1
+                if right+1<len(s):
+                    prefix[right+1]+=1
+            if dirn==1:
+                prefix[left] +=1
+                if right+1<len(s):
+                    prefix[right+1]+=-1
+        for j in range(1,len(prefix)):
+            prefix[j]+=prefix[j-1]
+        ans=[]
+        for k in range(len(s)):
+            letter=((ord(s[k])+prefix[k]-ord('a'))%26)+ord('a')
+            ans.append(chr(letter))
+        return "".join(ans)
+        
