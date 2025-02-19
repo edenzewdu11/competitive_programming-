2242fc131bda452b796f69e8ae7f9cd1eb9cab7a
Eden Zewdu <167537470+edenzewdu11@users.noreply.github.com>
2025-02-19 08:08:15 +0300

    Created 'D - Repeating Cipher' by A2SV Hub

---
+++ A2SV G6 - Round #1 19-Feb-2025/D - Repeating Cipher 272829.py
@@ -0,0 +1,14 @@
+# Problem: D - Repeating Cipher - https://codeforces.com/gym/585107/problem/D
+
+n = int(input())
+t = input()
+
+res = []
+i = 0
+for step in range(1, n + 1):
+    if i < n:
+        res.append(t[i])
+        i += step
+
+print("".join(res))
+
