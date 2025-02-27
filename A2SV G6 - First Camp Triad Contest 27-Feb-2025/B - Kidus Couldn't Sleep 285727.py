4f5984e293babc11ad62cba0d2633f4f0df12916
Eden Zewdu <167537470+edenzewdu11@users.noreply.github.com>
2025-02-27 09:40:10 +0300

    Created 'B - Kidus Couldn't Sleep' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 27-Feb-2025/B - Kidus Couldn't Sleep 285727.py
@@ -0,0 +1,11 @@
+# Problem: B - Kidus Couldn't Sleep - https://codeforces.com/gym/589822/problem/B
+
+n, k = map(int, input().split()) 
+a = list(map(int, input().split()))
+curr = sum(a[:k])
+total = curr
+for i in range(k, n):
+    curr += a[i] - a[i - k]
+    total += curr
+result=total / (n - k + 1)
+print(f"{result:.10f}")
