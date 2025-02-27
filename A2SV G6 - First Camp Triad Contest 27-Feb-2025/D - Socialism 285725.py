b52119dc7142ca3b98c4881cae36648730b75a12
Eden Zewdu <167537470+edenzewdu11@users.noreply.github.com>
2025-02-27 09:39:54 +0300

    Created 'D - Socialism' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 27-Feb-2025/D - Socialism 285725.py
@@ -0,0 +1,14 @@
+# Problem: D - Socialism - https://codeforces.com/gym/589822/problem/D
+
+t=int(input())
+for _ in range(t):
+    n,x=map(int,input().split())
+    a=sorted(map(int,input().split()),reverse=True)
+    summ=0
+    for i in range(n):
+        summ+=a[i]
+        if summ<x *(i+1):
+            print(i)
+            break
+    else:
+        print(n)
