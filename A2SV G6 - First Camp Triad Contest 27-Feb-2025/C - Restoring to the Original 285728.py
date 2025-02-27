02000e18077340bcc683a1a4080c8a94ef90e852
Eden Zewdu <167537470+edenzewdu11@users.noreply.github.com>
2025-02-27 09:40:18 +0300

    Created 'C - Restoring to the Original' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 27-Feb-2025/C - Restoring to the Original 285728.py
@@ -0,0 +1,13 @@
+# Problem: C - Restoring to the Original - https://codeforces.com/gym/589822/problem/C
+
+x=int(input())
+y=input()
+zero=[]
+one=[]
+for i in range(x):
+    if y[i]=="z":
+        zero.append(0)
+    elif y[i]=="n":
+        one.append(1)   
+one.extend(zero)
+print(*one)
