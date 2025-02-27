2d332b5fbbe9f3a36a109816d6e7c80eaa557ada
Eden Zewdu <167537470+edenzewdu11@users.noreply.github.com>
2025-02-27 09:53:52 +0300

    Created 'D - Minimize the Distance' by A2SV Hub

---
+++ A2SV G6 - Round #4 27-Feb-2025/D - Minimize the Distance 285733.py
@@ -0,0 +1,7 @@
+# Problem: D - Minimize the Distance - https://codeforces.com/gym/590053/problem/D
+
+n = int(input())
+a = list(map(int, input().split()))
+a.sort()
+minimum = a[(n-1)//2]
+print(minimum)
