b8f3a707fbb05afbc34e0d54fe8844e9823774e2
Eden Zewdu <167537470+edenzewdu11@users.noreply.github.com>
2025-02-27 09:55:23 +0300

    Created 'E - Equalizing Arrays' by A2SV Hub

---
+++ A2SV G6 - Round #3 27-Feb-2025/E - Equalizing Arrays 285735.py
@@ -0,0 +1,27 @@
+# Problem: E - Equalizing Arrays - https://codeforces.com/gym/588468/problem/E
+
+n = int(input())
+a = list(map(int, input().split()))
+m = int(input())
+b = list(map(int, input().split()))
+
+if sum(a) != sum(b):
+    print(-1)
+    exit()
+
+p1 = p2 = 0
+size = 0
+while p1 < n and p2 < m:
+    if a[p1] == b[p2]:
+        p1 += 1
+        p2 += 1
+        size += 1
+    while p1 < n-1 and a[p1] < b[p2]:
+        a[p1+1] += a[p1]
+        p1 += 1
+    while p2 < m-1 and a[p1] > b[p2]:
+        b[p2+1] += b[p2]
+        p2 += 1
+print(size)
+
+
