0f308ac8f1994ae26cda8728ab75c86ce8ff211e
Eden Zewdu <167537470+edenzewdu11@users.noreply.github.com>
2025-02-28 07:52:18 +0300

    Created 'Maximum Absolute Sum of Any Subarray' by A2SV Hub

---
+++ 28-Feb-2025/Maximum Absolute Sum of Any Subarray 286597.py
@@ -0,0 +1,19 @@
+# Problem: Maximum Absolute Sum of Any Subarray - https://leetcode.com/problems/maximum-absolute-sum-of-any-subarray/
+
+class Solution:
+    def maxAbsoluteSum(self, nums: List[int]) -> int:
+        maximum = nums[0]
+        maxEnding = nums[0]
+        for i in range(1, len(nums)):
+            maxEnding = max(maxEnding + nums[i], nums[i])
+            maximum = max(maximum, maxEnding)
+        minimum=nums[0]
+        minimumEnding=nums[0]
+        for i in range(1,len(nums)):
+            minimumEnding=min(minimumEnding+nums[i], nums[i])
+            minimum=min(minimum,minimumEnding)
+        ans=max(maximum,abs(minimum))
+        return ans 
+    
+        
+        
