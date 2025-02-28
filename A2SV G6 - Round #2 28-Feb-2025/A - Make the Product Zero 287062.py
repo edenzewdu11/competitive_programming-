cda0f9019f6bbc86f6ce62a7a723b7d09a632993
Eden Zewdu <167537470+edenzewdu11@users.noreply.github.com>
2025-02-28 19:15:45 +0300

    Created 'A - Make the Product Zero' by A2SV Hub

---
+++ A2SV G6 - Round #2 28-Feb-2025/A - Make the Product Zero 287062.py
@@ -0,0 +1,11 @@
+# Problem: A - Make the Product Zero - https://codeforces.com/gym/586960/problem/A
+
+x = int(input()) 
+a = list(map(int, input().split())) 
+
+
+if 0 in a:
+    print(0)
+else:
+    min_operations = min(abs(num) for num in a)
+    print(min_operations)
