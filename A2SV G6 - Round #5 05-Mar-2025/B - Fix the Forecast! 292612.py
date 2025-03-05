111b185f35fd2ada1c2fceb32c64baf3b9b8cb80
Eden Zewdu <167537470+edenzewdu11@users.noreply.github.com>
2025-03-05 10:31:34 +0300

    Created 'B - Fix the Forecast!' by A2SV Hub

---
+++ A2SV G6 - Round #5 05-Mar-2025/B - Fix the Forecast! 292612.py
@@ -0,0 +1,22 @@
+# Problem: B - Fix the Forecast! - https://codeforces.com/gym/591928/problem/B
+
+t = int(input())
+for _ in range(t):
+    n, k =  map(int, input().split())
+    a =  list(map(int, input().split()))
+    b =  list(map(int, input().split()))
+    
+    
+    a = [(a[i], i) for i in range(n)]
+    
+    
+    a.sort()
+    b.sort()
+    
+    
+    ans = [0] * n
+    for i in range(n):
+        _, ind = a[i]
+        ans[ind] = b[i]
+    
+    print(*ans)
