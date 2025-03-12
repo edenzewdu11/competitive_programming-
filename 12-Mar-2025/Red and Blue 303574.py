2c2bf9eb0a19648ff806e43278d4b6fc28f46801
Eden Zewdu <167537470+edenzewdu11@users.noreply.github.com>
2025-03-12 10:19:52 +0300

    Created 'Red and Blue' by A2SV Hub

---
+++ 12-Mar-2025/Red and Blue 303574.py
@@ -0,0 +1,21 @@
+# Problem: Red and Blue - https://codeforces.com/contest/1469/problem/B
+
+x = int(input())  
+
+for _ in range(x):
+    n = int(input())  
+    r = list(map(int, input().split()))
+    m = int(input())  
+    b = list(map(int, input().split()))
+
+    max_prefix_r, curr_r = 0, 0
+    for num in r:
+        curr_r += num
+        max_prefix_r = max(max_prefix_r, curr_r)
+
+    max_prefix_b, curr_b = 0, 0
+    for num in b:
+        curr_b += num
+        max_prefix_b = max(max_prefix_b, curr_b)
+
+    print(max_prefix_r + max_prefix_b)
