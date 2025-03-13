d5df44cd20b3ccd075c658b8b055ecac1913823d
Eden Zewdu <167537470+edenzewdu11@users.noreply.github.com>
2025-03-13 19:51:49 +0300

    Created '132 Pattern' by A2SV Hub

---
+++ 13-Mar-2025/132 Pattern 305475.py
@@ -0,0 +1,20 @@
+# Problem: 132 Pattern - https://leetcode.com/problems/132-pattern/
+
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        stack = []
+        temp = float('-inf')
+
+        for i in range(len(nums)-1, -1, -1):
+            if nums[i] < temp:
+                return True
+            
+            while stack and stack[-1] < nums[i]:
+                temp= stack.pop()
+
+            stack.append(nums[i])
+
+        return False 
+        
+        
+        
