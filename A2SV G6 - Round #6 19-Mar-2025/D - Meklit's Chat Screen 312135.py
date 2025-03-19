cbd93ee7fb97b9407ff25a34bdb684b5970a06a0
Eden Zewdu <167537470+edenzewdu11@users.noreply.github.com>
2025-03-19 10:48:54 +0300

    Created 'D - Meklit's Chat Screen' by A2SV Hub

---
+++ A2SV G6 - Round #6 19-Mar-2025/D - Meklit's Chat Screen 312135.py
@@ -0,0 +1,20 @@
+# Problem: D - Meklit's Chat Screen - https://codeforces.com/gym/594077/problem/D
+
+from collections import deque
+
+n, k = map(int, input().split())
+ids = list(map(int, input().split()))
+
+scr = deque()
+act = set()
+for frnd in ids:
+    if frnd in act:
+        continue
+    if len(scr) == k:
+        rem = scr.pop()
+        act.remove(rem)
+    scr.appendleft(frnd)
+    act.add(frnd)
+
+print(len(scr))
+print(*scr)
