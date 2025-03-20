22d352a4f42652b6640b90b7bee3748929e4b921
Eden Zewdu <167537470+edenzewdu11@users.noreply.github.com>
2025-03-20 22:38:30 +0300

    Created 'C - Binary Flip' by A2SV Hub

---
+++ A2SV G6 - Round #4 20-Mar-2025/C - Binary Flip 313721.py
@@ -0,0 +1,30 @@
+# Problem: C - Binary Flip - https://codeforces.com/gym/590053/problem/C
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = input()
+    b = input()
+    a += "0"
+    b += "0"
+    count =0
+    for i in range(n):
+        if a[i] == "1":
+            count += 1
+        else:
+            count -= 1
+
+        if count != 0:
+            if a[i] == b[i] and a[i+1] != b[i+1]:
+                print("NO")
+                break
+            elif a[i] != b[i] and a[i+1] == b[i+1]:
+                print("NO")
+                break
+    else:
+        print("YES")
+        
+            
+
+
+    
