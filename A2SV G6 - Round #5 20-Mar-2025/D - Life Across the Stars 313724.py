4a875ecbb1f8f3787f650615564e8794c8c96117
Eden Zewdu <167537470+edenzewdu11@users.noreply.github.com>
2025-03-20 22:40:35 +0300

    Created 'D - Life Across the Stars' by A2SV Hub

---
+++ A2SV G6 - Round #5 20-Mar-2025/D - Life Across the Stars 313724.py
@@ -0,0 +1,19 @@
+# Problem: D - Life Across the Stars - https://codeforces.com/gym/591928/problem/D
+
+from collections import defaultdict
+n=int(input())
+events=defaultdict(int)
+for _ in range(n):
+    b,d=map(int,input().split())
+    events[b]+=1
+    events[d]-=1
+years=sorted(events.keys())
+maxP=0
+year=0
+currentP=0
+for y in years:
+    currentP+=events[y]
+    if currentP>maxP:
+        maxP = currentP
+        year =y
+print(year,maxP)
