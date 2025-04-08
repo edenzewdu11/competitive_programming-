a2e9199537e2a97b138770634e72899fa212d492
Eden Zewdu <167537470+edenzewdu11@users.noreply.github.com>
2025-04-08 23:43:46 +0300

    Created 'Top K Frequent Elements' by A2SV Hub

---
+++ 08-Apr-2025/Top K Frequent Elements 329295.py
@@ -0,0 +1,24 @@
+# Problem: Top K Frequent Elements - https://leetcode.com/problems/top-k-frequent-elements/
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        frequency_map = defaultdict(int)
+        for num in nums:
+            frequency_map[num] += 1
+        n = len(nums)
+        buckets = [[] for _ in range(n + 1)]  
+        for num, freq in frequency_map.items():
+            buckets[freq].append(num)
+
+        
+        result = []
+        for i in range(n, 0, -1):  
+            if buckets[i]:
+                result.extend(buckets[i])
+            if len(result) >= k:
+                break
+
+        return result
+
+
+        
