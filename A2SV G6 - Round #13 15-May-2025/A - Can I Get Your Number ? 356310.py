ec4436fbf46a1935ce6072273ae4690f33a0461e
Eden Zewdu <167537470+edenzewdu11@users.noreply.github.com>
2025-05-15 18:25:52 +0300

    Created 'A - Can I Get Your Number ?' by A2SV Hub

---
+++ A2SV G6 - Round #13 15-May-2025/A - Can I Get Your Number ? 356310.py
@@ -0,0 +1,9 @@
+# Problem: A - Can I Get Your Number ? - https://codeforces.com/gym/607625/problem/A
+
+n = int(input())
+a = sorted([input().strip() for _ in range(n)])
+f_s, l_s = a[0], a[-1]
+i = 0
+while i < len(f_s) and f_s[i] == l_s[i]:
+    i += 1
+print(i)
