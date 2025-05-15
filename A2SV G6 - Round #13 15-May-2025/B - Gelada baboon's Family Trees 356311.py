2b2a4d49a0d68e643d199fdffd76dcab6866a9af
Eden Zewdu <167537470+edenzewdu11@users.noreply.github.com>
2025-05-15 18:26:06 +0300

    Created 'B - Gelada baboon's Family Trees' by A2SV Hub

---
+++ A2SV G6 - Round #13 15-May-2025/B - Gelada baboon's Family Trees 356311.py
@@ -0,0 +1,24 @@
+# Problem: B - Gelada baboon's Family Trees - https://codeforces.com/gym/607625/problem/B
+
+from collections import defaultdict
+n = int(input())
+p = list(map(int, input().split()))
+graph = defaultdict(list)
+for i in range(n):
+    graph[i+1].append(p[i])
+    graph[p[i]].append(i+1)
+visited =set()
+def dfs(node):
+    visited.add(node)
+    for neigh in graph[node]:
+        if neigh  not in visited:
+            dfs(neigh)
+component=0
+for i in range(n):
+    if i+1 not in visited:
+        dfs(i+1)
+        component+=1
+print(component)
+
+
+
